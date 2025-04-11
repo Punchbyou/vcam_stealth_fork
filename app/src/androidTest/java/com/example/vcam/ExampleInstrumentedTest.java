c62401a05ceb5d6cbc74dbd5b75642af7dae5330
Punchbyou <160106217+Punchbyou@users.noreply.github.com>
2025-04-12 06:44:20 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/vcam/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/vcam/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.vcam;
+package package com.android.framework.cache;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.vcam", appContext.getPackageName());
+        assertEquals("package com.android.framework.cache", appContext.getPackageName());
     }
 }
